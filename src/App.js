a423a3f49a49083f75f12ae6215ea2606a3edf1b
Marco <marco_trabajo@yahoo.com>
2022-04-03 20:57:09 +0200

    project setup and sidebar completed, not responsive

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import styled from 'styled-components'
+import SideBar from './Components/Sidebar'
+import Dashboard from './Components/Dashboard'
+import Earnings from './Components/Earnings'
+import Navbar from './Components/Navbar'
+import Profile from './Components/Profile'
+import ReusableStyles from './Components/ReusableStyles'
+import Transfers from './Components/Transfers'
+import FAQ from './Components/FAQ'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Div>
+      <SideBar />
+      <Dashboard />
+      {/* <Earnings />
+      <Navbar />
+      <Profile />
+      <ReusableStyles />
+      <Transfers />
+      <FAQ /> */}
 
-export default App;
+    </Div>
+  )
+}
+const Div = styled.div``;
+
+
+
+export default App
