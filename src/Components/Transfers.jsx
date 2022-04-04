0881b8dcb86e41b0281017a9e5e5ab38c3c1075d
Marco <marco_trabajo@yahoo.com>
2022-04-04 13:45:50 +0200

    transfers section completed, images, icons and animations

--- src/Components/Transfers.jsx
+++ src/Components/Transfers.jsx
@@ -1,7 +1,134 @@
 import React from 'react'
+import styled from 'styled-components'
+import { HiArrowNarrowRight } from 'react-icons/hi'
+import avatarImage from '../assets/avatarImage.jpeg'
+import { cardStyle } from './ReusableStyles'
 
 export default function Transfers() {
+  const transactions = [
+    {
+      image: avatarImage,
+      name: "From Kishan Sheth",
+      time: "Today, 12:30",
+      amount: "+$50",
+    },
+    {
+      image: avatarImage,
+      name: "To Laura Santos",
+      time: "Today, 08:49",
+      amount: "-$25",
+    },
+    {
+      image: avatarImage,
+      name: "From Jadon S.",
+      time: "Yesterday, 14:36",
+      amount: "+$150",
+    },
+  ]
   return (
-    <div>Transfers</div>
+    <Section>
+      <div className="title">
+        <h2>Your Transfers</h2>
+      </div>
+      <div className="transactions">
+        { transactions.map((transaction) => {
+          return (
+            <div className="transaction">
+              <div className="transaction__title">
+                <div className="transaction__title__image">
+                  <img src={transaction.image}  alt="transaction image" />
+                </div>
+                <div className="transaction__title__details">
+                  <h3>{transaction.name}</h3>
+                  <h5>{transaction.time}</h5>
+                </div>
+              </div>
+              <div className="transaction__amount">
+                <span>{transaction.amount}</span>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+      <a href="#" className="view">View all <HiArrowNarrowRight /></a>
+    </Section>
   )
 }
+
+const Section = styled.section`
+  ${cardStyle}
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  /* start .title  */
+  .title {
+    h2 {
+      color: #ffc107;
+      font-family: "Permanent Marker", cursive;
+      letter-spacing: 0.3rem; 
+    }
+  }
+  /* start .title  */
+  /* start transactions  */
+  .transactions {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    margin-top: 1rem;
+    .transaction {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      &__title {
+        display: flex;
+        gap: 1rem;
+        &__image {
+          img {
+            height: 2.5rem;
+            border-radius: 3rem; 
+          }
+        }
+      }
+      &__amount {
+        background-color: #d7e41e1d;
+        padding: 0.2rem 0.5rem;
+        width: 4rem;
+        border-radius: 1rem;
+        text-align: center;
+        transition: 0.3s ease-in-out;
+        &:hover {
+          background-color: #ffc107;
+          span {
+            color: black;
+          }
+        }
+        span {
+          color: #ffc107;
+        }
+      }
+    }
+  }  
+  /* end transactions  */
+  /* start .view  */
+  .view {
+    width: 100%;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    text-decoration: none;
+    color: #ffc107;
+    font-weight: bold;
+    margin-top: 1rem;
+    gap: 0.5rem;
+    svg {
+      transition: 0.3s ease-in-out;
+      font-size: 1.4rem;
+    }
+    &:hover {
+      svg {
+        transform: translateX(0.5rem);
+      }
+    }
+  }
+  /* end .view  */
+`;
